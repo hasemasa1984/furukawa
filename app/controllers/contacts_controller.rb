a6bd27167bf1ce6b2a5d241018aae58c0031e07a
hasemasa1984 <107299485+hasemasa1984@users.noreply.github.com>
2022-12-19 20:59:31 +0900

    プライベートメソッドとnewルーティングメソッド

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,9 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
+
+  private
+  def contact_params
+    params.require(:Contact).permit(:name, :email)
+  end
 end
